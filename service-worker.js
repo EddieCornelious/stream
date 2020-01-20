41a508d285e8925cbb13758cf5a3d46c920f01e2
EddieCornelious <eulerissmart@gmail.com>
2020-01-20 08:49:52 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stream/precache-manifest.b1defea8c2476ba34f3ada311f1bbc12.js"
+  "/stream/precache-manifest.3520ad34334d0494679c246fde6edb5a.js"
 );
 
 workbox.clientsClaim();
