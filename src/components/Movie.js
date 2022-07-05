e2c8ca17e333fe667411de26ba02fe9acad1f0f4
Eddie <eulerissmart@gmail.com>
2022-07-05 16:41:03 -0400

    added show movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,13 +3,17 @@ import '../styles/Movie.scss';
 import { ReactComponent as Star } from '../star-solid.svg';
 import bg from '../test.jpg';
 import { ReactComponent as House } from '../house-solid.svg';
+import { ReactComponent as LeftArrow } from '../left-long-solid.svg';
+import { ReactComponent as RightCaret } from '../caret-right-solid.svg';
+import { ReactComponent as Trailer } from '../video-solid.svg';
 
 export default function Movie(props) {
   return (
     <div className="movie__bg">
+      <div className="blur">lol</div>
       <div className="movie__poster">
         <div className="movie__poster__title">
-          <h1>sonic the hedgehog</h1>
+          <h1>sonic the hedgehog 2</h1>
           <span className="movie__poster__rating">
             <Star />
             <Star />
@@ -24,26 +28,31 @@ export default function Movie(props) {
         </div>
         <div className="movie__poster__body">
           <div className="movie__poster__body__top">
+            <h3 className="quote">"welcome to the next level"</h3>
             <span>
               Action / Science Fiction / Comedy / Family / 2022-03-30 / 122min
             </span>
             <a href="#">Full Cast</a>
             <div className="movie__poster__body__btn__group">
-              <a href="#">
+              <a id="home" href="#">
                 <House />
               </a>
-              <a href="#">watch trailer</a>
+              <a id="trailer" href="#">
+                <Trailer />
+              </a>
             </div>
           </div>
 
           <div className="movie__poster__body__bottom">
-            After settling in Green Hills, Sonic is eager to prove he has what
-            it takes to be a true hero. His test comes when Dr. Robotnik
-            returns, this time with a new partner, Knuckles, in search for an
-            emerald that has the power to destroy civilizations. Sonic teams up
-            with his own sidekick, Tails, and together they embark on a
-            globe-trotting journey to find the emerald before it falls into the
-            wrong hands.
+            <p>
+              After settling in Green Hills, Sonic is eager to prove he has what
+              it takes to be a true hero. His test comes when Dr. Robotnik
+              returns, this time with a new partner, Knuckles, in search for an
+              emerald that has the power to destroy civilizations. Sonic teams
+              up with his own sidekick, Tails, and together they embark on a
+              globe-trotting journey to find the emerald before it falls into
+              the wrong hands.
+            </p>
           </div>
         </div>
       </div>
